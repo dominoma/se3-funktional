5fd7cf5a789f5c79e4550ec9b3f22df5ec8ed5c5
Fabian Holst <fabian.holst@studium.uni-hamburg.de>
2019-11-30 11:12:04 +0100

    Added comments

--- Blatt05/Aufgabenblatt 5.rkt
+++ Blatt05/Aufgabenblatt 5.rkt
@@ -3,26 +3,37 @@
 
 #|schmetterling: '(dominant '(farbe musterung fühlerform flügelform)
                    rezessiv '(farbe musterung fühlerform flügelform))|#
+
+;; Dominanzregeln, vordere Einträge dominieren hintere
 (define dominanz-musterung '(star dots stripes))
 (define dominanz-farbe '(red green blue yellow))
 (define dominanz-fuehler '( curved straight curly))
 (define dominanz-fluegel '(ellipse hexagon rhomb ))
 
+;;Gibt für zwei übergebene Gene ein Paar zurück,
+;;in dem das dominante Gen an erster Stelle steht, das rezessive an zweiter
+;;als erster Parameter wird eine Dominanzliste übergeben.
+;;Gen1 und Gen2 müssen in dieser Liste vorkommen
 (define (dominanz regeln gen1 gen2)
   (if (< (index-of regeln gen1) (index-of regeln gen2))
       (cons gen1 gen2)
       (cons gen2 gen1)
       )
   )
-(define (pair-struct schmett)
+
+;;Wandelt einen Schmetterling in eine Liste von (dominant . rezessiv) Paaren
+;;Seiner Gene um
+(define (schmetterling->listofgenpairs schmett)
   (map cons (car schmett) (cdr schmett))
-  )
-#| |#
-(define (listofgenpairs->pairofgenlist listofgenpairs)
+)
+
+;;Wandelt eine Liste von (dominant . rezessiv) Paaren in einen
+;;Schmetterling um
+(define (listofgenpairs->schmetterling listofgenpairs)
   (cons (map car listofgenpairs) (list (map cdr listofgenpairs)))
   )
                                                        
-
+;;Wählt von einem Gen-Paar zufällig eines aus
 (define (rand-gen 2-gene)
   (if (< (random) 0.5)
       (car 2-gene)
@@ -30,8 +41,9 @@
       )
   )
 (define (liebe-machen mutter vater)
-  [let ((kindUnordered (cons (map rand-gen mutter)
-  (map rand-gen vater))))
+  [let (
+        (kindUnordered
+         (cons (map rand-gen mutter) (map rand-gen vater))))
     (list (dominanz dominanz-farbe (caar kindUnordered) (cadr kindUnordered))
    (dominanz dominanz-musterung (cadar kindUnordered) (caddr kindUnordered))
    (dominanz dominanz-fuehler (caddar kindUnordered) (cadddr kindUnordered))
